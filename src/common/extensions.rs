a4070ca35b8e658603167d1a7ef26c76210a67c0
dbolivar25 <danielbolivarflorez@gmail.com>
2024-08-28 17:37:35 -0500

    added tests for extensions

--- src/common/extensions.rs
+++ src/common/extensions.rs
@@ -72,3 +72,68 @@ impl<T> PureOptionExt<T> for Option<T> {
         Some(t)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ensure_or() {
+        let result = Ok(42);
+        let predicate = |value: &i32| *value == 42;
+        let error = "Value is not 42";
+
+        assert_eq!(result.ensure_or(predicate, error), Ok(42));
+
+        let result = Ok(43);
+        assert_eq!(result.ensure_or(predicate, error), Err("Value is not 42"));
+
+        let result = Err("Error");
+        assert_eq!(result.ensure_or(predicate, error), Err("Error"));
+    }
+
+    #[test]
+    fn test_ensure_or_else() {
+        let result = Ok(42);
+        let predicate = |value: &i32| *value == 42;
+        let error = |value: &i32| format!("Value is not 42, but {}", value);
+
+        assert_eq!(result.ensure_or_else(predicate, error), Ok(42));
+
+        let result = Ok(43);
+        assert_eq!(
+            result.ensure_or_else(predicate, error),
+            Err("Value is not 42, but 43".to_string())
+        );
+
+        let result = Err("Error".to_string());
+        assert_eq!(
+            result.ensure_or_else(predicate, error),
+            Err("Error".to_string())
+        );
+    }
+
+    #[test]
+    fn test_ensure() {
+        let option = Some(42);
+        let predicate = |value: &i32| *value == 42;
+
+        assert_eq!(option.ensure(predicate), Some(42));
+
+        let option = Some(43);
+        assert_eq!(option.ensure(predicate), None);
+
+        let option = None;
+        assert_eq!(option.ensure(predicate), None);
+    }
+
+    #[test]
+    fn test_pure_result() {
+        assert_eq!(Result::<i32, &str>::pure(42), Ok(42));
+    }
+
+    #[test]
+    fn test_pure_option() {
+        assert_eq!(Option::<i32>::pure(42), Some(42));
+    }
+}
