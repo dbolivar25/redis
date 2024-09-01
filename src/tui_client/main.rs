9db07146834c85645ced4414ea1aa9f78ab53f91
dbolivar25 <danielbolivarflorez@gmail.com>
2024-08-31 22:49:25 -0500

    made tui prompt pretty. added command completions.

--- src/tui_client/main.rs
+++ src/tui_client/main.rs
@@ -9,10 +9,55 @@ use redis::{
     },
     tui_client::cli::Args,
 };
-use rustyline::{error::ReadlineError, DefaultEditor};
 use std::error::Error;
 use tokio::{net::TcpStream, time::Instant};
 use tokio_util::codec::Framed;
+
+use std::borrow::Cow::{self, Borrowed, Owned};
+
+use rustyline::{
+    completion::FilenameCompleter,
+    error::ReadlineError,
+    highlight::{Highlighter, MatchingBracketHighlighter},
+    hint::HistoryHinter,
+    Completer, CompletionType, Config, EditMode, Editor, Helper, Hinter, Validator,
+};
+
+#[derive(Helper, Completer, Hinter, Validator)]
+struct RedisTUIHelper {
+    #[rustyline(Completer)]
+    completer: FilenameCompleter,
+    highlighter: MatchingBracketHighlighter,
+    #[rustyline(Hinter)]
+    hinter: HistoryHinter,
+    colored_prompt: String,
+}
+
+impl Highlighter for RedisTUIHelper {
+    fn highlight_prompt<'b, 's: 'b, 'p: 'b>(
+        &'s self,
+        prompt: &'p str,
+        default: bool,
+    ) -> Cow<'b, str> {
+        if default {
+            Borrowed(&self.colored_prompt)
+        } else {
+            Borrowed(prompt)
+        }
+    }
+
+    fn highlight_hint<'h>(&self, hint: &'h str) -> Cow<'h, str> {
+        Owned("\x1b[1m".to_owned() + hint + "\x1b[m")
+    }
+
+    fn highlight<'l>(&self, line: &'l str, pos: usize) -> Cow<'l, str> {
+        self.highlighter.highlight(line, pos)
+    }
+
+    fn highlight_char(&self, line: &str, pos: usize, forced: bool) -> bool {
+        self.highlighter.highlight_char(line, pos, forced)
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -21,10 +66,26 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let tcp = TcpStream::connect(addr).await?;
     let (mut sink, mut stream) = Framed::new(tcp, RESP3Codec).split();
 
-    let mut rl = DefaultEditor::new()?;
+    let config = Config::builder()
+        .history_ignore_space(true)
+        .completion_type(CompletionType::List)
+        .edit_mode(EditMode::Vi)
+        .build();
+    let h = RedisTUIHelper {
+        completer: FilenameCompleter::new(),
+        highlighter: MatchingBracketHighlighter::new(),
+        hinter: HistoryHinter::new(),
+        colored_prompt: "".to_owned(),
+        // validator: MatchingBracketValidator::new(),
+    };
+
+    let mut rl = Editor::with_config(config)?;
+    rl.set_helper(Some(h));
 
     loop {
-        let readline = rl.readline("redis> ");
+        let p = "redis> ";
+        rl.helper_mut().expect("No helper").colored_prompt = format!("\x1b[1;32m{p}\x1b[0m");
+        let readline = rl.readline(&p);
 
         match readline {
             Ok(line) => {
